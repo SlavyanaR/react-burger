8676b39358440d99a9b6e980859d046ae2150da0
SlavyanaR <95542891+SlavyanaR@users.noreply.github.com>
2022-09-13 00:32:28 +0300

    4

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import AppHeader from '../AppHeader/AppHeader';
+
+function App () {
+    return (
+        <div>
+            <AppHeader/>
+        </div>
+    )
+}
+export default App;
